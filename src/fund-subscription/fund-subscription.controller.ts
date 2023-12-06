70084064b110b84891c674eb7319a071458910da
ruberwa <ruberwa3@gmail.com>
2023-12-06 12:26:13 +0200

    Manager to get All Subscription

--- src/fund-subscription/fund-subscription.controller.ts
+++ src/fund-subscription/fund-subscription.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   Patch,
   Post,
@@ -201,11 +203,11 @@ export class FundSubscriptionByManagerController {
     const subscriptions = await this.subscriptionService.getAllSubscriptions(
       managerId,
     );
-
+  
     if (subscriptions.length === 0) {
-      return { message: 'There are no subscriptions available.' };
+      throw new HttpException('There are no subscriptions available.', HttpStatus.NOT_FOUND);
     }
-
+  
     return subscriptions;
   }
 
